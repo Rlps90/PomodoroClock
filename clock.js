6924b8fda4b7bc78ab0ee867732d98723ab761a3
Renan Carvalho <rcrvlh90@gmail.com>
2020-11-09 16:43:25 -0300

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,25 +1,39 @@
-var tempoAtual = document.getElementById("numClock").value * 60
-var minutos = parseInt(document.getElementById("numClock").value, 10)
+let tempoAtual = document.getElementById("numClock").value * 60
+let minutos = parseInt(document.getElementById("numClock").value, 10)
+const botaoIniciar = document.getElementById("start")
+const botaoPausar = document.getElementById("pause")
+const botaoReset = document.getElementById("reset")
 let isPaused = true
 
 //Início do relógio Pomodoro ao clicar start
 if (tempoAtual > 0) {
-    document.getElementById("start").addEventListener("click", () => myVar = setInterval(() => {
+    botaoIniciar.addEventListener("click", function play() {
+            myVar = setInterval(() => {
 
-        let minRestante = Math.floor(tempoAtual / 60)
-        let segRestante = tempoAtual - minRestante * 60
+                let minRestante = Math.floor(tempoAtual / 60)
+                let segRestante = tempoAtual - minRestante * 60
 
-        tempoAtual--
+                tempoAtual--
 
-        document.getElementById("timer").innerHTML = duasCasas(minRestante, 2) + ":" + duasCasas(segRestante, 2)
-    }, 1000))
+                document.getElementById("timer").innerHTML = duasCasas(minRestante, 2) + ":" + duasCasas(segRestante, 2)
+            }, 1000)
+    })
 } else if (tempoAtual === 0 && document.getElementById("start").clicked == true) {
     document.getElementById("timer").innerHTML = "Hora do descanço!"
 }
 
-document.getElementById("pause").addEventListener("click", () => {
-    clearInterval(myVar)
-})
+if (isPaused){
+    botaoPausar.addEventListener("click", () => {
+        clearInterval(myVar)
+        isPaused = false
+    })
+} else {
+    botaoPausar.addEventListener("click", () => {
+        play()
+        isPaused = true
+    })
+}
+
 
 function reset() {
 
